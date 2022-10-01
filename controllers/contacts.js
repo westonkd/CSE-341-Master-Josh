1a92e0f6a3438fdf553f11b7acc1243457fd61e0
Weston Dransfield <wdrans@gmail.com>
2022-10-01 16:21:15 -0600

    udpate DB name

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,19 +1,27 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db('CSE341').collection('contacts').find();
+  const result = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
 
 const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db('CSE341').collection('contacts').find({ _id: userId });
+  const result = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
@@ -24,13 +32,21 @@ const createContact = async (req, res) => {
     lastName: req.body.lastName,
     email: req.body.email,
     favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    birthday: req.body.birthday,
   };
-  const response = await mongodb.getDb().db('CSE341').collection('contacts').insertOne(contact);
+  const response = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .insertOne(contact);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the contact.');
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while creating the contact."
+      );
   }
 };
 
@@ -42,29 +58,41 @@ const updateContact = async (req, res) => {
     lastName: req.body.lastName,
     email: req.body.email,
     favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
+    birthday: req.body.birthday,
   };
   const response = await mongodb
     .getDb()
-    .db('CSE341')
-    .collection('contacts')
+    .db("cs_341_projects")
+    .collection("contacts")
     .replaceOne({ _id: userId }, contact);
   console.log(response);
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the contact.');
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while updating the contact."
+      );
   }
 };
 
 const deleteContact = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const response = await mongodb.getDb().db('CSE341').collection('contacts').remove({ _id: userId }, true);
+  const response = await mongodb
+    .getDb()
+    .db("cs_341_projects")
+    .collection("contacts")
+    .remove({ _id: userId }, true);
   console.log(response);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the contact.');
+    res
+      .status(500)
+      .json(
+        response.error || "Some error occurred while deleting the contact."
+      );
   }
 };
 
@@ -73,5 +101,5 @@ module.exports = {
   getSingle,
   createContact,
   updateContact,
-  deleteContact
+  deleteContact,
 };
